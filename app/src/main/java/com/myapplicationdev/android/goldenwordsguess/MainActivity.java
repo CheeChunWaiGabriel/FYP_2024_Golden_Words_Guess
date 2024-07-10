15ce668f5a9c95c008563a6cf9076a0ef0935bae
22022050 <22022050@myrp.edu.sg>
2024-07-10 16:36:30 +0800

    Added testing code for a sample puzzle and shortcut from homescreen.

--- app/src/main/java/com/myapplicationdev/android/goldenwordsguess/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/goldenwordsguess/MainActivity.java
@@ -1,6 +1,10 @@
 package com.myapplicationdev.android.goldenwordsguess;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button testbutton=findViewById(R.id.shotcutbutton);
+testbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Puzzlecode.class);
+                startActivity(intent);
+
+            }
         });
     }
 }
