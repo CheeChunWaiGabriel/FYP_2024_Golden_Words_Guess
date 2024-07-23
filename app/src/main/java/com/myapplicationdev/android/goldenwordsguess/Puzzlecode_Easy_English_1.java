46a975025459e9a16cf28a57d13f6f363e642705
22022050 <22022050@myrp.edu.sg>
2024-07-23 14:42:07 +0800

    Added functionality for all easy puzzles

--- app/src/main/java/com/myapplicationdev/android/goldenwordsguess/Puzzlecode_Easy_English_1.java
+++ app/src/main/java/com/myapplicationdev/android/goldenwordsguess/Puzzlecode_Easy_English_1.java
@@ -8,18 +8,31 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class Puzzlecode_Easy_English_1 extends AppCompatActivity {
 
     private TextView[] boxes;
+    private Button[] letterButtons;
     private int currentBoxIndex = 0;
     private final String correctWord = "WOLF";
+
+    private Button btnTryAgain;
+    private Button btnHome;
+    private Button btnUndo;
+    private ImageView resultIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puzzle_layout_easy_english_1);
 
+        initializeViews();
+        setLetterButtonListeners();
+        setControlButtonListeners();
+    }
+
+    private void initializeViews() {
         boxes = new TextView[]{
                 findViewById(R.id.box1),
                 findViewById(R.id.box2),
@@ -27,72 +40,138 @@ public class Puzzlecode_Easy_English_1 extends AppCompatActivity {
                 findViewById(R.id.box4)
         };
 
-        Button btnTop = findViewById(R.id.btn_letter_top);
-        Button btnLeft = findViewById(R.id.btn_letter_left);
-        Button btnRight = findViewById(R.id.btn_letter_right);
-        Button btnBottom = findViewById(R.id.btn_letter_bottom);
-        Button btnTryAgain = findViewById(R.id.btn_try_again);
-        Button btnHome = findViewById(R.id.btn_home);
-        ImageView resultIndicator = findViewById(R.id.result_indicator);
+        letterButtons = new Button[]{
+                findViewById(R.id.btn_letter_top),
+                findViewById(R.id.btn_letter_left),
+                findViewById(R.id.btn_letter_right),
+                findViewById(R.id.btn_letter_bottom)
+        };
 
+        btnTryAgain = findViewById(R.id.btn_try_again);
+        btnHome = findViewById(R.id.btn_home);
+        btnUndo = findViewById(R.id.btn_undo);
+        resultIndicator = findViewById(R.id.result_indicator);
+    }
+
+    private void setLetterButtonListeners() {
         View.OnClickListener letterClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (currentBoxIndex < boxes.length) {
                     Button button = (Button) view;
-                    boxes[currentBoxIndex].setText(button.getText());
-                    currentBoxIndex++;
-
-                    if (currentBoxIndex == boxes.length) {
-                        verifyWord(resultIndicator, btnTryAgain);
-                    }
+                    handleLetterSelection(button);
                 }
             }
         };
 
-        btnTop.setOnClickListener(letterClickListener);
-        btnLeft.setOnClickListener(letterClickListener);
-        btnRight.setOnClickListener(letterClickListener);
-        btnBottom.setOnClickListener(letterClickListener);
+        for (Button letterButton : letterButtons) {
+            letterButton.setOnClickListener(letterClickListener);
+        }
+    }
 
+    private void handleLetterSelection(Button button) {
+        boxes[currentBoxIndex].setText(button.getText());
+        button.setTextColor(ContextCompat.getColor(this, R.color.grey));
+        button.setEnabled(false);
+        verifyLetter(button.getText().toString(), currentBoxIndex);
+        currentBoxIndex++;
+    }
+
+    private void verifyLetter(String letter, int index) {
+        if (correctWord.charAt(index) == letter.charAt(0)) {
+            boxes[index].setBackgroundColor(ContextCompat.getColor(this, R.color.correct_letter_color));
+            if (index == boxes.length - 1) {
+                showResultIndicator(R.drawable.correct, resultIndicator);
+            }
+        } else {
+            boxes[index].setBackgroundColor(ContextCompat.getColor(this, R.color.wrong_letter_color));
+            showResultIndicator(R.drawable.wrong, resultIndicator);
+            btnTryAgain.setVisibility(View.VISIBLE);
+            btnUndo.setVisibility(View.VISIBLE);
+            disableUnselectedLetterButtons();
+        }
+    }
+
+    private void showResultIndicator(int drawableRes, ImageView resultIndicator) {
+        resultIndicator.setImageResource(drawableRes);
+        resultIndicator.setVisibility(View.VISIBLE);
+    }
+
+    private void setControlButtonListeners() {
         btnTryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                resetGame(resultIndicator, btnTryAgain);
+                resetGame();
             }
         });
 
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Puzzlecode_Easy_English_1.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(Puzzlecode_Easy_English_1.this, MainActivity.class));
+            }
+        });
+
+        btnUndo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                undoLastMove();
             }
         });
     }
 
-    private void verifyWord(ImageView resultIndicator, Button btnTryAgain) {
-        StringBuilder formedWord = new StringBuilder();
-        for (TextView box : boxes) {
-            formedWord.append(box.getText().toString());
-        }
-
-        if (formedWord.toString().equals(correctWord)) {
-            resultIndicator.setImageResource(R.drawable.correct);
-        } else {
-            resultIndicator.setImageResource(R.drawable.wrong);
-            btnTryAgain.setVisibility(View.VISIBLE);
-        }
-        resultIndicator.setVisibility(View.VISIBLE);
-    }
-
-    private void resetGame(ImageView resultIndicator, Button btnTryAgain) {
+    private void resetGame() {
         for (TextView box : boxes) {
             box.setText("");
+            box.setBackgroundResource(R.drawable.box_border); // Reset to original drawable
         }
+
         currentBoxIndex = 0;
         resultIndicator.setVisibility(View.GONE);
         btnTryAgain.setVisibility(View.GONE);
+        btnUndo.setVisibility(View.GONE);
+
+        for (Button letterButton : letterButtons) {
+            letterButton.setEnabled(true);
+            letterButton.setTextColor(ContextCompat.getColor(this, R.color.black));
+        }
+    }
+
+    private void undoLastMove() {
+        if (currentBoxIndex > 0) {
+            currentBoxIndex--;
+            TextView lastBox = boxes[currentBoxIndex];
+            String lastLetter = lastBox.getText().toString();
+            lastBox.setText("");
+            lastBox.setBackgroundResource(R.drawable.box_border); // Reset to original drawable
+
+            for (Button letterButton : letterButtons) {
+                if (letterButton.getText().toString().equals(lastLetter)) {
+                    letterButton.setEnabled(true);
+                    letterButton.setTextColor(ContextCompat.getColor(this, R.color.black));
+                    break;
+                }
+            }
+
+            resultIndicator.setVisibility(View.GONE);
+            btnTryAgain.setVisibility(View.GONE);
+            btnUndo.setVisibility(View.GONE);
+
+            enableAllLetterButtons();
+        }
+    }
+
+    private void disableUnselectedLetterButtons() {
+        for (Button letterButton : letterButtons) {
+            if (letterButton.isEnabled()) {
+                letterButton.setEnabled(false);
+            }
+        }
+    }
+
+    private void enableAllLetterButtons() {
+        for (Button letterButton : letterButtons) {
+            letterButton.setEnabled(true);
+        }
     }
 }
-
--- app/src/main/java/com/myapplicationdev/android/goldenwordsguess/Puzzlecode_Easy_English_3.java
+++ app/src/main/java/com/myapplicationdev/android/goldenwordsguess/Puzzlecode_Easy_English_3.java
@@ -8,10 +8,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class Puzzlecode_Easy_English_3 extends AppCompatActivity {
 
     private TextView[] boxes;
+    private Button[] letterButtons;
     private int currentBoxIndex = 0;
     private final String correctWord = "FROG";
 
@@ -20,6 +22,12 @@ public class Puzzlecode_Easy_English_3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puzzle_layout_easy_english_3);
 
+        initializeViews();
+        setLetterButtonListeners();
+        setControlButtonListeners();
+    }
+
+    private void initializeViews() {
         boxes = new TextView[]{
                 findViewById(R.id.box1),
                 findViewById(R.id.box2),
@@ -27,72 +35,147 @@ public class Puzzlecode_Easy_English_3 extends AppCompatActivity {
                 findViewById(R.id.box4)
         };
 
-        Button btnTop = findViewById(R.id.btn_letter_top);
-        Button btnLeft = findViewById(R.id.btn_letter_left);
-        Button btnRight = findViewById(R.id.btn_letter_right);
-        Button btnBottom = findViewById(R.id.btn_letter_bottom);
-        Button btnTryAgain = findViewById(R.id.btn_try_again);
-        Button btnHome = findViewById(R.id.btn_home);
-        ImageView resultIndicator = findViewById(R.id.result_indicator);
+        letterButtons = new Button[]{
+                findViewById(R.id.btn_letter_top),
+                findViewById(R.id.btn_letter_left),
+                findViewById(R.id.btn_letter_right),
+                findViewById(R.id.btn_letter_bottom)
+        };
+    }
 
+    private void setLetterButtonListeners() {
         View.OnClickListener letterClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (currentBoxIndex < boxes.length) {
                     Button button = (Button) view;
-                    boxes[currentBoxIndex].setText(button.getText());
-                    currentBoxIndex++;
-
-                    if (currentBoxIndex == boxes.length) {
-                        verifyWord(resultIndicator, btnTryAgain);
-                    }
+                    handleLetterSelection(button);
                 }
             }
         };
 
-        btnTop.setOnClickListener(letterClickListener);
-        btnLeft.setOnClickListener(letterClickListener);
-        btnRight.setOnClickListener(letterClickListener);
-        btnBottom.setOnClickListener(letterClickListener);
+        for (Button letterButton : letterButtons) {
+            letterButton.setOnClickListener(letterClickListener);
+        }
+    }
+
+    private void handleLetterSelection(Button button) {
+        boxes[currentBoxIndex].setText(button.getText());
+        button.setTextColor(ContextCompat.getColor(this, R.color.grey));
+        button.setEnabled(false);
+        verifyLetter(button.getText().toString(), currentBoxIndex);
+        currentBoxIndex++;
+    }
+
+    private void verifyLetter(String letter, int index) {
+        ImageView resultIndicator = findViewById(R.id.result_indicator);
+        Button btnTryAgain = findViewById(R.id.btn_try_again);
+        Button btnUndo = findViewById(R.id.btn_undo);
+
+        if (correctWord.charAt(index) == letter.charAt(0)) {
+            boxes[index].setBackgroundColor(ContextCompat.getColor(this, R.color.correct_letter_color));
+            if (index == boxes.length - 1) {
+                showResultIndicator(R.drawable.correct, resultIndicator);
+            }
+        } else {
+            boxes[index].setBackgroundColor(ContextCompat.getColor(this, R.color.wrong_letter_color));
+            showResultIndicator(R.drawable.wrong, resultIndicator);
+            btnTryAgain.setVisibility(View.VISIBLE);
+            btnUndo.setVisibility(View.VISIBLE);
+            disableUnselectedLetterButtons();
+        }
+    }
+
+    private void showResultIndicator(int drawableRes, ImageView resultIndicator) {
+        resultIndicator.setImageResource(drawableRes);
+        resultIndicator.setVisibility(View.VISIBLE);
+    }
+
+    private void setControlButtonListeners() {
+        Button btnTryAgain = findViewById(R.id.btn_try_again);
+        Button btnHome = findViewById(R.id.btn_home);
+        Button btnUndo = findViewById(R.id.btn_undo);
 
         btnTryAgain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                resetGame(resultIndicator, btnTryAgain);
+                resetGame();
             }
         });
 
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Puzzlecode_Easy_English_3.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(Puzzlecode_Easy_English_3.this, MainActivity.class));
+            }
+        });
+
+        btnUndo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                undoLastMove();
             }
         });
     }
 
-    private void verifyWord(ImageView resultIndicator, Button btnTryAgain) {
-        StringBuilder formedWord = new StringBuilder();
-        for (TextView box : boxes) {
-            formedWord.append(box.getText().toString());
-        }
+    private void resetGame() {
+        ImageView resultIndicator = findViewById(R.id.result_indicator);
+        Button btnTryAgain = findViewById(R.id.btn_try_again);
+        Button btnUndo = findViewById(R.id.btn_undo);
 
-        if (formedWord.toString().equals(correctWord)) {
-            resultIndicator.setImageResource(R.drawable.correct);
-        } else {
-            resultIndicator.setImageResource(R.drawable.wrong);
-            btnTryAgain.setVisibility(View.VISIBLE);
-        }
-        resultIndicator.setVisibility(View.VISIBLE);
-    }
-
-    private void resetGame(ImageView resultIndicator, Button btnTryAgain) {
         for (TextView box : boxes) {
             box.setText("");
+            box.setBackgroundResource(R.drawable.box_border); // Reset to original drawable
         }
+
         currentBoxIndex = 0;
         resultIndicator.setVisibility(View.GONE);
         btnTryAgain.setVisibility(View.GONE);
+        btnUndo.setVisibility(View.GONE);
+
+        for (Button letterButton : letterButtons) {
+            letterButton.setEnabled(true);
+            letterButton.setTextColor(ContextCompat.getColor(this, R.color.black));
+        }
+    }
+
+    private void undoLastMove() {
+        if (currentBoxIndex > 0) {
+            currentBoxIndex--;
+            TextView lastBox = boxes[currentBoxIndex];
+            String lastLetter = lastBox.getText().toString();
+            lastBox.setText("");
+            lastBox.setBackgroundResource(R.drawable.box_border); // Reset to original drawable
+
+            for (Button letterButton : letterButtons) {
+                if (letterButton.getText().toString().equals(lastLetter)) {
+                    letterButton.setEnabled(true);
+                    letterButton.setTextColor(ContextCompat.getColor(this, R.color.black));
+                    break;
+                }
+            }
+
+            ImageView resultIndicator = findViewById(R.id.result_indicator);
+            resultIndicator.setVisibility(View.GONE);
+            findViewById(R.id.btn_try_again).setVisibility(View.GONE);
+            findViewById(R.id.btn_undo).setVisibility(View.GONE);
+
+            enableAllLetterButtons();
+        }
+    }
+
+    private void disableUnselectedLetterButtons() {
+        for (Button letterButton : letterButtons) {
+            if (letterButton.isEnabled()) {
+                letterButton.setEnabled(false);
+            }
+        }
+    }
+
+    private void enableAllLetterButtons() {
+        for (Button letterButton : letterButtons) {
+            letterButton.setEnabled(true);
+        }
     }
 }
 
